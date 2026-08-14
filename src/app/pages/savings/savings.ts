1acdfa0567816bd8eb9331cd36f208d9c48a2bb7
vaniel56 <olamiadadejumo@email.com>
2026-08-14 17:01:46 +0100

    savings working fine only persentage functionality left

--- src/app/pages/savings/savings.ts
+++ src/app/pages/savings/savings.ts
@@ -1,9 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, signal, OnInit } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
+import { Router } from '@angular/router';
+import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-savings',
-  imports: [],
+  imports: [DecimalPipe, FormsModule],
   templateUrl: './savings.html',
-  styleUrl: './savings.css',
+  styleUrls: ['./savings.css'],
 })
-export class Savings {}
+export class Savings implements OnInit {
+  savingspage: string = 'savingsdetails-page';
+  savings = signal(0);
+  savingsAmount!: string;
+  amount = Number(this.savingsAmount);
+  insufficientFunds = false
+
+  constructor(private router: Router) {}
+  
+  // `router` is now injected via the constructor
+
+  ngOnInit() {
+    const savedSavings = localStorage.getItem('savings');
+    if (savedSavings) {
+      this.savings.set(Number(savedSavings));
+    }
+  }
+  goToSucceful() {
+    const amount = Number(this.savingsAmount); // Convert string to number
+    const currentBalance = Number(this.savings()); // Get the current balance
+    if (!this.savingsAmount || Number(this.savingsAmount) <= 0) {
+      alert('Enter valid amount');
+      return;
+    }
+    this.savings.set(this.savings() - amount);
+    // persist updated balance
+    localStorage.setItem('savings', this.savings().toString());
+    this.saveTransaction();
+
+    this.savingspage = 'success-page';
+  }
+
+  goToHome() {
+    this.router.navigate(['layout/home']);
+  }
+  saveTransaction() {
+    const transactions = JSON.parse(localStorage.getItem('transactions') || '[]');
+
+    transactions.push({
+      beneficiary: 'Added to new laptop fund',
+      amount: Number(this.savingsAmount),
+      date: new Date().toISOString(),
+      type: 'savings',
+    });
+
+    localStorage.setItem('transactions', JSON.stringify(transactions));
+
+    console.log('Transaction saved:', transactions);
+  }
+  checkBalance() {
+    const amount = Number(this.savingsAmount);
+
+    if (amount > Number(this.savings()) && amount > 0) {
+      this.insufficientFunds = true;
+    } else {
+      this.insufficientFunds = false;
+    }
+  }
+}
